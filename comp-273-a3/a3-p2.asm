4002ad3bad16626cc2b9d608c5a0f2c31f50b755
Simon Zheng <simz97@outlook.com>
2017-11-20 23:23:48 -0500

    Completed first draft

--- comp-273-a3/a3-p2.asm
+++ comp-273-a3/a3-p2.asm
@@ -5,12 +5,28 @@
 ###########################################################
 .data
 error: .asciiz "error: m, n must be non-negative integers\n"
+prompt1: .asciiz "Enter m\n"
+prompt2: .asciiz "Enter n\n"
 
 .text 
 ###########################################################
 main:
 # get input from console using syscalls
-
+	li $v0, 4
+	la $a0, prompt1
+	syscall
+	
+	li $v0, 5		# Read m
+	syscall
+	add $a0,$v0,$zero	# Move m to $a0
+	
+	li $v0, 4
+	la $a0, prompt2
+	syscall
+	
+	li $v0, 5		# Read n
+	syscall
+	add $a1,$v0,$zero	# Move n to $a1
 # compute A on inputs 
 	jal A
 # print value to console and exit with status 0
@@ -22,13 +38,16 @@ A:
 	add $t0,$a0,$zero	# Put "m" into $t0
 	add $t1,$a1,$zero	# Put "n" into $t1
 	
-Base:	beq $t0,0,End		# If m = 0 go to return
-	blt $t0,0,End		# If m < 0 go to return (just in case)
+Check1:	bne $t0,0,Check2	# If m != 0 then go to next check else if m = 0 then go to return n + 1
+	add $v0,$t1,1		# Return n + 1
+	j End
+Check2	blt $t0,0,End		# If m < 0 go to return (just in case)
 	
-Mgt0:	addi $sp,$sp,-12	# Add space on stack for 3 words
+Mgt0:	addi $sp,$sp,-16	# Add space on stack for 4 words
 	sw $t0,0($sp)		# Store m on stack
 	sw $t1,4($sp)		# Store n on stack
-	sw $ra,8($sp)		# Store $ra on stack
+	sw $v0,8($v0)		# Store $v0 on stack
+	sw $ra,12($sp)		# Store $ra on stack
 	
 Nis0:	bne $t1,0,Ngt0		# Else if n != 0 then go to Ngt0 instead
 	
@@ -42,20 +61,24 @@ Ngt0:	bne $t1,0,Ngt0		# Else if not n > 0 then End
 	
 	add $a0,$t0,$zero	# $a0 is m
 	addi $a1,$t1,-1		# $a1 is n - 1
+	#add $s0,$v0,$zero	# Save current $v0 (necessary??)
 	jal A			# Recursive call
 	
 	add $t2,$v0,$zero	# Move result into $t2
+	lw $v0,8($sp)		# Restore $v0
 	lw $t0,0($sp)		# Get current m back into $t0 from stack
 	addi $a0,$t0,-1		# $a0 is m - 1
 	addi $a1,$t2,$zero	# $a1 is $t2
 	jal A			# Recursive call
 	
-RecEnd:	lw $ra,0($sp)		# Load $ra from stack
-	addi $sp,$sp,4		# Move stack pointer back
+RecEnd:	lw $ra,0($sp)		# Load m from stack
+	lw $ra,4($sp)		# Load n from stack
+	#lw $v0,8($sp)		# Load $v0 from stack
+	lw $ra,12($sp)		# Load $ra from stack
+	addi $sp,$sp,12		# Move stack pointer back down
 	
-End:	add $v0,$t1,1		# Return n + 1
-	jal check
-	jr $ra
+End:	#jal check
+	jr $ra			# Result in $v0
 	
 ###########################################################
 # void check(int m, int n)
