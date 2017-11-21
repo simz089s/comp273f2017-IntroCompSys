b88ba7cb3bc7a5371f73fe0446a068999397066c
Simon Zheng <simz97@outlook.com>
2017-11-21 18:46:04 -0500

    A3Q3 I can't believe it works.

--- comp-273-a3/a3-p3.asm
+++ comp-273-a3/a3-p3.asm
@@ -5,9 +5,9 @@
 # Numerical Integration with the Floating Point Coprocessor
 ###########################################################
 .data
-N: .word 100
-a: .float 0
-b: .float 1
+N: .word 1000
+a: .float 100
+b: .float 200
 error: .asciiz "error: must have low < hi\n"
 
 .text 
@@ -21,7 +21,7 @@ main:
 	jal integrate
 	# print result and exit with status 0
 	li $v0,2
-	#mov.s $f0,$f0
+	mov.s $f12,$f0
 	syscall
 	li $v0,17		# Exit 0
 	xor $a0,$a0,$a0
@@ -48,12 +48,12 @@ integrate:
 	mov.s $f24,$f4		# Save N into $f24
 	mov.s $f26,$f12		# Save starting x_i (x_0 = a) into $f26
 	addi $t0,$zero,2	# Get 2 to divide by 2
-	mtc1 $f6,$t0		# Move to float processors
+	mtc1 $t0,$f6		# Move to float processors
 	cvt.s.w $f6,$f6		# Convert to float
 	div.s $f6,$f20,$f6	# Divide D(x) by 2 and put into $f6
 	add.s $f26,$f26,$f6	# x_i + D(x)/2
 	addi $t0,$zero,1	# Get 1 to increment
-	mtc1 $f6,$t0		# Move to float processors
+	mtc1 $t0,$f6		# Move to float processors
 	cvt.s.w $f30,$f6	# Convert to float and put in $f30
 	
 Loop:	c.eq.s $f22,$f24	# Check if i = N
