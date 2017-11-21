bb6f06ef491cf439723bd234fe427ee189ce9943
Simon Zheng <simz97@outlook.com>
2017-11-20 23:58:43 -0500

    A3Q2 Fixed everything, check left

--- comp-273-a3/a3-p2.asm
+++ comp-273-a3/a3-p2.asm
@@ -12,13 +12,13 @@ prompt2: .asciiz "Enter n\n"
 ###########################################################
 main:
 # get input from console using syscalls
-	li $v0, 4
+	li $v0,4
 	la $a0, prompt1
 	syscall
 	
 	li $v0,5		# Read m
 	syscall
-	add $a0,$v0,$zero	# Move m to $a0
+	add $t0,$v0,$zero	# Move m to $t0
 	
 	li $v0,4
 	la $a0, prompt2
@@ -26,8 +26,10 @@ main:
 	
 	li $v0,5		# Read n
 	syscall
-	add $a1,$v0,$zero	# Move n to $a1
+	add $t1,$v0,$zero	# Move n to $t1
 # compute A on inputs 
+	add $a0,$t0,$zero
+	add $a1,$t1,$zero
 	jal A
 # print value to console and exit with status 0
 	add $a0,$v0,$zero	# Print result
@@ -45,15 +47,13 @@ A:
 	add $t1,$a1,$zero	# Put "n" into $t1
 	
 Check1:	bne $t0,0,Check2	# If m != 0 then go to next check else if m = 0 then go to return n + 1
-	add $v0,$t1,1		# Return n + 1
+	add $v0,$t1,1		# If m = 0 then return n + 1
 	j End
-Check2:	blt $t0,0,End		# If m < 0 go to return (just in case)
+Check2:	blt $t0,0,End		# If m < 0 then go to return (just in case)
 	
-Mgt0:	addi $sp,$sp,-16	# Add space on stack for 4 words
+Mgt0:	addi $sp,$sp,-8		# Add space on stack for 4 words
 	sw $t0,0($sp)		# Store m on stack
-	sw $t1,4($sp)		# Store n on stack
-	sw $v0,8($sp)		# Store $v0 on stack
-	sw $ra,12($sp)		# Store $ra on stack
+	sw $ra,4($sp)		# Store $ra on stack
 	
 Nis0:	bne $t1,0,Ngt0		# Else if n != 0 then go to Ngt0 instead
 	
@@ -70,17 +70,13 @@ Ngt0:	blt $t1,0,End		# Else if not n > 0 then End
 	jal A			# Recursive call
 	
 	add $t2,$v0,$zero	# Move result into $t2
-	#lw $v0,8($sp)		# Restore $v0 from stack
 	lw $t0,0($sp)		# Get current m back into $t0 from stack
 	addi $a0,$t0,-1		# $a0 is m - 1
 	add $a1,$t2,$zero	# $a1 is $t2
 	jal A			# Recursive call
 	
-RecEnd:	#lw $t0,0($sp)		# Load m from stack
-	#lw $t1,4($sp)		# Load n from stack
-	#lw $v0,8($sp)		# Load $v0 from stack
-	lw $ra,12($sp)		# Load $ra from stack
-	addi $sp,$sp,16		# Move stack pointer back down
+RecEnd:	lw $ra,4($sp)		# Load $ra from stack
+	addi $sp,$sp,8		# Move stack pointer back down
 	
 End:	#jal check
 	jr $ra			# Result in $v0
