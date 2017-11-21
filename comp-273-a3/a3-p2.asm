df0ba3501186c31f3feb0b1191d8b625775ec98e
Simon Zheng <simz97@outlook.com>
2017-11-21 00:31:46 -0500

    A3Q2 Implemented check and tested everything works!

--- comp-273-a3/a3-p2.asm
+++ comp-273-a3/a3-p2.asm
@@ -13,7 +13,7 @@ prompt2: .asciiz "Enter n\n"
 main:
 # get input from console using syscalls
 	li $v0,4
-	la $a0, prompt1
+	la $a0,prompt1
 	syscall
 	
 	li $v0,5		# Read m
@@ -21,7 +21,7 @@ main:
 	add $t0,$v0,$zero	# Move m to $t0
 	
 	li $v0,4
-	la $a0, prompt2
+	la $a0,prompt2
 	syscall
 	
 	li $v0,5		# Read n
@@ -46,14 +46,20 @@ A:
 	add $t0,$a0,$zero	# Put "m" into $t0
 	add $t1,$a1,$zero	# Put "n" into $t1
 	
-Check1:	bne $t0,0,Check2	# If m != 0 then go to next check else if m = 0 then go to return n + 1
-	add $v0,$t1,1		# If m = 0 then return n + 1
-	j End
-Check2:	blt $t0,0,End		# If m < 0 then go to return (just in case)
-	
-Mgt0:	addi $sp,$sp,-8		# Add space on stack for 4 words
+	addi $sp,$sp,-12	# Add space on stack for 4 words
 	sw $t0,0($sp)		# Store m on stack
-	sw $ra,4($sp)		# Store $ra on stack
+	sw $t1,4($sp)		# Store n on stack
+	sw $ra,8($sp)		# Store $ra on stack
+	
+Check:	jal check		# Check if m and n are natural numbers
+	beq $v0,$zero,RecEnd	# If m is not a natural number then go to return
+	
+	lw $t0,0($sp)		# Restore m from stack
+	lw $t1,4($sp)		# Restore n from stack
+	
+Check1:	bne $t0,0,Nis0		# If m != 0 then go to recursive case
+	add $v0,$t1,1		# If m = 0 then return n + 1
+	j RecEnd
 	
 Nis0:	bne $t1,0,Ngt0		# Else if n != 0 then go to Ngt0 instead
 	
@@ -75,15 +81,29 @@ Ngt0:	blt $t1,0,End		# Else if not n > 0 then End
 	add $a1,$t2,$zero	# $a1 is $t2
 	jal A			# Recursive call
 	
-RecEnd:	lw $ra,4($sp)		# Load $ra from stack
-	addi $sp,$sp,8		# Move stack pointer back down
+RecEnd:	lw $ra,8($sp)		# Restore $ra from stack
+	addi $sp,$sp,12		# Move stack pointer back down
 	
-End:	#jal check
-	jr $ra			# Result in $v0
+End:	jr $ra			# Result in $v0
 	
 ###########################################################
 # void check(int m, int n)
 # checks that n, m are natural numbers
 # prints error message and exits with status 1 on fail
 check:
-	jr $ra
+	add $t0,$a0,$zero	# Put "m" into $t0
+	add $t1,$a1,$zero	# Put "n" into $t1
+	
+	xor $v0,$v0,$v0		# Initially assume false
+	
+CheckM:	blt $t0,$zero,Error	# If m is not natural then print error and return false else check n
+CheckN: blt $t1,$zero,Error	# If n is not natural then print error and return false else return true
+	addi $v0,$zero,1
+	j Checked
+	
+Error:	li $v0,4
+	la $a0,error
+	syscall
+	j Checked
+	
+Checked:jr $ra
