a1b870973d382c0db6d503bfea53168d72a1cea3
Simon Zheng <simz97@outlook.com>
2017-11-21 18:39:54 -0500

    A3Q3 Tried something

--- comp-273-a3/a3-p3.asm
+++ comp-273-a3/a3-p3.asm
@@ -34,6 +34,7 @@ main:
 # $f0 gets return value
 integrate: 
 	add $s0,$ra,$zero	# Save return address
+	add $s1,$a0,$zero	# Save function address
 	jal check
 	
 	# initialize $f4 to hold N
@@ -43,23 +44,33 @@ integrate:
 	sub.s $f6,$f13,$f12	# Put b-a into $f6 temp reg
 	div.s $f20,$f6,$f4	# Put delta(x)=(b-a)/N into $f20
 	mtc1 $zero,$f22		# Set i counter to 0
-	mov.s $f24,$f12		# Save a into $f24
-	mov.s $f26,$f4		# Save N into $f24
+	mtc1 $zero,$f28		# Set sum to 0 in $f28
+	mov.s $f24,$f4		# Save N into $f24
+	mov.s $f26,$f12		# Save starting x_i (x_0 = a) into $f26
+	addi $t0,$zero,2	# Get 2 to divide by 2
+	mtc1 $f6,$t0		# Move to float processors
+	cvt.s.w $f6,$f6		# Convert to float
+	div.s $f6,$f20,$f6	# Divide D(x) by 2 and put into $f6
+	add.s $f26,$f26,$f6	# x_i + D(x)/2
+	addi $t0,$zero,1	# Get 1 to increment
+	mtc1 $f6,$t0		# Move to float processors
+	cvt.s.w $f30,$f6	# Convert to float and put in $f30
 	
-Loop:	c.eq.s $f24,$f26	# Check if i = N
+Loop:	c.eq.s $f22,$f24	# Check if i = N
 	bc1t Done		# Finish if i = N
 	
 	# Calculate midpoint D(x)/2+(a+x_0?) and then just keep adding D(x) (is counter i even needed?)
-	#mul.s $f6,$f22,$f20	# Calculate x_i into $f6 (i * D(x))
-	#add.s $f6,$f24,$f6	# a + i * D(x)
-	#add.s $f22,$f22,1	# Increment i
-	#mul.s $f8,$f22,$f20	# Calculate x_i+1 into $f8 (i * D(x))
-	#add.s $f8,$f24,$f6	# a + i * D(x)
-	#sub.s $f10,$f8
-	jalr $a0		# Jump to function
+	mov.s $f12,$f26		# Calculate mid point of x_i
+	jalr $s1		# Jump to function
+	mul.s $f6,$f20,$f0	# D(x) * f(x_i) into $f6
+	add.s $f28,$f28,$f6	# sum = sum + $f6
 	
+	add.s $f22,$f22,$f30	# Increment i
+	add.s $f26,$f26,$f20	# Increment x_i by D(x)
+	j Loop
 	
-Done:	add $ra,$s0,$zero	# Restore return address
+Done:	mov.s $f0,$f28		# Return sum
+	add $ra,$s0,$zero	# Restore return address
 	jr $ra
 
 ###########################################################
